76e484907fc6718e2a8bfce92f34d768dfcbedea
Juncheng Han <namrie1990@gmail.com>
2017-04-24 16:12:48 -0700

    Fizz and Buzz

--- String.playground/Contents.swift
+++ String.playground/Contents.swift
@@ -424,7 +424,41 @@ func firstUniqChar(_ s: String) -> Int {
     return -1
 }
 
+/*:
+ ## Fizz and Buzz
+ Write a program that outputs the string representation of numbers from 1 to n.
+ 
+ But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+ 
+ */
 
+func fizzAndBuzz(_ n: Int) -> [String] {
+    
+    var res = [String]()
+    
+    if n <= 0 {
+        return res
+    }
+    
+    for i in 1...n {
+        var temp = ""
+        if i % 3 == 0 && i % 5 == 0 {
+            temp.append("FizzBuzz")
+        } else if i % 3 == 0 {
+            temp.append("Fizz")
+        } else if i % 5 == 0 {
+            temp.append("Buzz")
+        } else {
+            temp.append(String(i))
+        }
+        
+        res.append(temp)
+    }
+    
+    return res
+}
+
+// fizzAndBuzz(15)
 
 
 
