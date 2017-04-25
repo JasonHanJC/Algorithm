7e076e4a0d05155c025825c7c59150a9787360ce
Juncheng Han <namrie1990@gmail.com>
2017-04-24 17:34:47 -0700

    flip game

--- String.playground/Contents.swift
+++ String.playground/Contents.swift
@@ -460,5 +460,44 @@ func fizzAndBuzz(_ n: Int) -> [String] {
 
 // fizzAndBuzz(15)
 
+/*: 
+ ## Flip Game
+ You are playing the following Flip Game with your friend: Given a string that contains only these two characters: + and -, you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can no longer make a move and therefore the other person will be the winner.
+ 
+ Write a function to compute all possible states of the string after one valid move.
+ 
+ For example, given s = "++++", after one move, it may become one of the following states:
+ 
+ [
+ "--++",
+ "+--+",
+ "++--"
+ ]
+ 
+ */
 
+func flipGame(_ s: String) -> [String] {
+    var res = [String]()
+    
+    var chars = Array(s.characters)
+    
+    guard chars.count > 0 else {
+        return res
+    }
+    
+    for i in 0..<(chars.count - 1) {
+        
+        if chars[i] == "+" && chars[i+1] == "+" {
+            chars[i] = "-"
+            chars[i + 1] = "-"
+            
+            res.append(String(chars))
+            chars[i] = "+"
+            chars[i + 1] = "+"
+        }
+    }
+    
+    return res
+}
 
+//flipGame("")
