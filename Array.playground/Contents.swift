7ab540c7587207bda584fc9d79b47116af31826f
Juncheng Han <namrie1990@gmail.com>
2017-05-03 01:47:59 -0700

    Island Perimeter

--- Array.playground/Contents.swift
+++ Array.playground/Contents.swift
@@ -1065,7 +1065,7 @@ func majorityElementII(_ nums: [Int]) -> [Int] {
     return res
 }
 
-majorityElementII([0,-1,2,-1])
+//majorityElementII([0,-1,2,-1])
 
 
 /*:
@@ -1115,8 +1115,60 @@ func longestConsecutive(_ nums: [Int]) -> Int {
     return longest
 }
 
-longestConsecutive([0, 1, 2, 3, 4])
+//longestConsecutive([0, 1, 2, 3, 4])
 
+
+/*:
+ ## Island Perimeter
+ You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water. Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells). The island doesn't have "lakes" (water inside that isn't connected to the water around the island). One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+ 
+ Example:
+ 
+ [[0,1,0,0],
+ [1,1,1,0],
+ [0,1,0,0],
+ [1,1,0,0]]
+ 
+ Answer: 16
+ Explanation: The perimeter is the 16 yellow stripes in the image below:
+ ![island.png](island.png)
+ 
+ Idae: The default single island has 4 Perimeter, check its left and top neighbors, if it has, minus number of neighbors * 2
+ */
+
+
+func islandPerimeter(_ grid: [[Int]]) -> Int {
+    let n = grid.count
+    let m = grid[0].count
+    var res = 0
+    
+    for i in 0..<n {
+        for j in 0..<m {
+            
+            if grid[i][j] == 0 {
+                continue
+            } else {
+                
+                res += 4
+                // top neighbor
+                if i - 1 >= 0 && grid[i - 1][j] == 1 {
+                    res -= 2
+                }
+                
+                if j - 1 >= 0 && grid[i][j - 1] == 1 {
+                    res -= 2
+                }
+            }
+        }
+    }
+    
+    return res
+}
+
+//islandPerimeter([[0,1,0,0],
+                 [1,1,1,0],
+                 [0,1,0,0],
+                 [1,1,0,0]])
 
 
 
