4c91ad2598e224db0759b2034540e902731fe61d
Juncheng Han <namrie1990@gmail.com>
2017-05-14 21:50:18 -0700

    Merge Two Sorted Lists & Merge k Sorted Lists

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -77,13 +77,74 @@ func reverseList(_ head: ListNode?) -> ListNode? {
         dummyhead.next = head
         
         head = next
-    
     }
     
     
     return dummyhead.next
 }
 
+/*:
+ ## Merge Two Sorted Lists
+ Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+ 
+*/
+
+func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+    
+    let dummyHead = ListNode(0)
+    var p = dummyHead
+    
+    var l1 = l1
+    var l2 = l2
+    
+    while l1 != nil && l2 != nil {
+        
+        if l1!.val > l2!.val {
+            p.next = l2!
+            l2 = l2!.next
+        } else {
+            p.next = l1!
+            l1 = l1!.next
+        }
+        
+        p = p.next!
+    }
+    
+    p.next = l1 ?? l2
+    
+    return dummyHead.next
+}
+
+/*:
+ ## Merge k Sorted Lists
+ 
+ Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ 
+ Idea: Divide and conquer
+ The idea is to pair up K lists and merge each pair in linear time using O(1) space. After first cycle, K/2 lists are left each of size 2*N. After second cycle, K/4 lists are left each of size 4*N and so on. We repeat the procedure until we have only one list left.
+ 
+ */
+
+func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
+    
+    guard lists.count > 0 else {
+        return nil
+    }
+    
+    var left = 0
+    var right = lists.count - 1
+    
+    while right > 0 {
+        left = 0
+        while left < right {
+            lists[left] = mergeTwoLists(lists[left], lists[right])
+            left += 1
+            right -= 1
+        }
+    }
+    
+    return lists[0]
+}
 
 
 
