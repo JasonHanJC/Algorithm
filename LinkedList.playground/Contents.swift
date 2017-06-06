938e4899141c768c2882684169527c48ecb06cb5
Juncheng Han <namrie1990@gmail.com>
2017-06-05 23:13:25 -0700

    Reverse Nodes in k-Group

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -418,3 +418,58 @@ func reorderList(_ head: ListNode?) {
     }
 }
 
+/*:
+ ## Reverse Nodes in k-Group
+ Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+ 
+ k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+ 
+ You may not alter the values in the nodes, only nodes itself may be changed.
+ 
+ Only constant memory is allowed.
+ 
+ For example,
+ Given this linked list: 1->2->3->4->5
+ 
+ For k = 2, you should return: 2->1->4->3->5
+ 
+ For k = 3, you should return: 3->2->1->4->5
+
+ */
+
+func reverseKGroup(_ head: ListNode?, _ k: Int) -> ListNode? {
+    
+    if head == nil || head!.next == nil {
+        return head
+    }
+    
+    let dummyHead = ListNode(0)
+    dummyHead.next = head
+    var p = dummyHead
+    var prev = head, post = head
+    
+    while post != nil {
+        
+        var i = 0
+        while post != nil && i != k - 1 {
+            post = post!.next
+            i += 1
+        }
+        
+        if i != k - 1 || post == nil {
+            break
+        }
+        
+        let rest = post!.next
+        post!.next = nil
+        
+        p.next = reverseList(prev)
+        p = prev!
+        p.next = rest
+        prev = rest
+        post = rest
+    }
+    
+    return dummyHead.next
+}
+
